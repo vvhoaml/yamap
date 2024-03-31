96cdf123a8665c772705e51b5dc65c9a23ba1f83
whoami <lallipapi4@yandex.ru>
2024-04-01 09:17:18 +1000

    Исправил ошибку с отображением меток, теперь они меняют цвет

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
 var myMap;
+var objectManager;
 
 ymaps.ready(init);
 
 function init () {
     myMap = new ymaps.Map('map', {
-            center: [55.76, 37.64],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        objectManager = new ymaps.ObjectManager({
-            clusterize: true,
-            gridSize: 32,
-            clusterDisableClickZoom: true
-        });
+        center: [55.76, 37.64],
+        zoom: 10
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
 
-    objectManager.objects.options.set('preset', 'islands#redDotIcon');
-    objectManager.clusters.options.set('preset', 'islands#redClusterIcons');
+    objectManager = new ymaps.ObjectManager({
+        clusterize: true,
+        gridSize: 32,
+        clusterDisableClickZoom: true
+    });
+
     myMap.geoObjects.add(objectManager);
-	myMap.controls.remove('searchControl');
-	myMap.controls.remove('trafficControl');
-	myMap.controls.remove('fullscreenControl');
+    myMap.controls.remove('searchControl');
+    myMap.controls.remove('trafficControl');
+    myMap.controls.remove('fullscreenControl');
 
     $.ajax({
         url: "objects.json"
     }).done(function(data) {
         objectManager.add(data);
     });
-
 }
 
 function changeStatus(id) {
-    var currentStatus = document.getElementById('status_' + id).innerText.trim();
+    var feature = objectManager.objects.getById(id);
+    if (feature) {
+        if (feature.properties.status === "свободно") {
+            feature.properties.status = "занято";
+            objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    		objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+        } else {
+            feature.properties.status = "свободно";
+            objectManager.objects.options.set('preset', 'islands#redDotIcon');
+    		objectManager.clusters.options.set('preset', 'islands#redClusterIcons');
+        }
+        objectManager.objects.balloon.open(id);
+        objectManager.objects.balloon.setData(feature.properties);
+		newStatus = feature.properties.status;
+        
+        objectManager.objects.setObjectOptions(id, feature);
+        
+        objectManager.objects.balloon.close();
+        objectManager.objects.balloon.open(id);
+        objectManager.objects.balloon.setData(feature.properties);
 
-    var newState = currentStatus === 'свободно' ? 'занято' : 'свободно';
-    
-    $.ajax({
-        type: 'POST',
-        url: 'updateStatus.php',
-        data: {
-            id: id,
-            newState: newState
-        },
-        success: function(response) {
-            if (response.success) {
-                document.getElementById('status_' + id).innerText = newState;
-
-                var feature = myMap.geoObjects.getById(id);
-                feature.properties.set('status', newState);
-
-                var newFeature = new ymaps.GeoObject(feature.geometry, {
-                    properties: feature.properties,
-                    options: feature.options
-                });
-
-                myMap.geoObjects.remove(feature);
-                myMap.geoObjects.add(newFeature);
-            }
-        },
-    });
+        $.ajax({
+			url: "updateStatus.php",
+			type: "POST",
+			contentType: "application/json",
+			data: JSON.stringify({
+				id: id,
+				status: newStatus
+			}),
+			success: function(response) {
+				console.log("Данные сохранены");
+			},
+			error: function(xhr, status, error) {
+				console.error("Ошибка при сохранении данных");
+			}
+		});
+    }
 }
--- updateStatus.php
+++ updateStatus.php
@@ -1,15 +1,34 @@
 <?php
 
-$id = $_POST['id'];
-$newState = $_POST['newState']; 
+header('Content-Type: application/json; charset=utf-8');
 
-$currentData = json_decode(file_get_contents('objects.json'), true);
+$postData = json_decode(file_get_contents('php://input'), true);
 
-$currentData['features'][$id]['properties']['status'] = $newState;
+if (!isset($postData['id']) || !isset($postData['status'])) {
+    echo json_encode(array('success' => false, 'error' => 'Недостаточно данных для обновления', 'received_data' => $postData));
+    exit;
+}
 
-$currentData = json_encode($currentData, JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_PRETTY_PRINT);
+$id = $postData['id'];
+$status = $postData['status'];
+$jsonFile = 'objects.json';
+$jsonData = json_decode(file_get_contents($jsonFile), true);
 
-file_put_contents('objects.json', $currentData);
+if (empty($jsonData) || !isset($jsonData['features'])) {
+    echo json_encode(array('success' => false, 'error' => 'Ошибка чтения данных из JSON файла'));
+    exit;
+}
 
-echo json_encode(array('success' => true));
+foreach ($jsonData['features'] as &$feature) {
+    if ($feature['id'] == $id) {
+        $feature['properties']['status'] = $status;
+        break;
+    }
+}
+
+if (file_put_contents($jsonFile, json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)) !== false) {
+    echo json_encode(array('success' => true));
+} else {
+    echo json_encode(array('success' => false, 'error' => 'Ошибка записи в файл'));
+}
 ?>
